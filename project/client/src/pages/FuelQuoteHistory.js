1d687f7fb4c2b630f68ea3e1163b7566ff1e375f
DavidF1998 <92688005+DavidF1998@users.noreply.github.com>
2022-02-16 19:18:55 -0600

    Update FuelQuoteHistory.js

--- project/client/src/pages/FuelQuoteHistory.js
+++ project/client/src/pages/FuelQuoteHistory.js
@@ -13,6 +13,47 @@ function FuelQuoteHistory() {
       <button onClick={togglePopup} className={styles.button_form}>Create New Form</button>
       {isOpen && <Popup handleClose={togglePopup} />}
     </div>
+       
+        <div>
+        <h1> Fuel Qoute History </h1> 
+        <span>
+          <table className = { styles.table } >
+            <thead>
+              <tr>
+                <th> Gallons Requested </th> 
+                <th> Delivery Address </th> 
+                <th> Delivery Date </th> 
+                <th > Price / Gallon </th>
+                <th> Total Amount Paid </th> 
+              </tr> 
+            </thead> 
+            <tbody>
+              <tr>
+                <th> 2 </th> 
+                <th> 123 Gallon Blvd </th> 
+                <th> 1 / 12 / 2022 </th> 
+                <th> 2.01 $ </th> 
+                <th> 4.02 $ </th> 
+              </tr> 
+              <tr>
+                <th> 15 </th> 
+                <th> 432 Full Tank Road </th> 
+                <th> 2 / 13 / 2022 </th> 
+                <th> 2.88 $ </th> 
+                <th> 42.00 $ </th> 
+              </tr> 
+              <tr>
+                <th> 3 </th> 
+                <th> 777 Needed Gas St. </th> 
+                <th> 12 / 12 / 2021 </th> 
+                <th> 3.91 $ </th> 
+                <th> 11.73 $ </th>
+              </tr> 
+            </tbody> 
+          </table > 
+        </span> 
+        </div> 
+        </div>
   );
 }
 
