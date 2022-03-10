da4123a8fb9dcb4be84d31890c7e69a71613e710
usman-hoosein <69318943+usman-hoosein@users.noreply.github.com>
2022-03-09 20:50:28 -0600

    test

--- project/server/app.js
+++ project/server/app.js
@@ -1,17 +1,17 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 //TODO: update when database is complete
-// const pool = require('./util/database'); 
+// const pool = require('./util/database');
 
 const app = express();
 
-const loginRoutes = require('./routes/login');
-const profileRoutes = require('./routes/client-profile');
-const fuelRoutes = require('./routes/fuel-quote');
+const loginRoutes = require("./routes/login");
+const profileRoutes = require("./routes/client-profile");
+const fuelRoutes = require("./routes/fuel-quote");
 
 //TODO: update when database is complete
 // pool.query("SET SCHEMA '';", (err, res) => {
@@ -23,12 +23,12 @@ const fuelRoutes = require('./routes/fuel-quote');
 app.use(express.json());
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/login', loginRoutes);
-app.use('/profile', profileRoutes)
-app.use('/fuel', fuelRoutes);
+app.use("/login", loginRoutes);
+app.use("/profile", profileRoutes);
+app.use("/fuel", fuelRoutes);
 
 const PORT = process.env.PORT || 8080;
-  
+
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
